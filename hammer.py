2515989bff53fed16a028a06a89b2c6eec95a2d4
dorkitude <dork@dorkitude.com>
2010-09-30 14:13:17 -0700

    readme for more clarity

--- hammer.py
+++ hammer.py
@@ -7,24 +7,6 @@ LOG_FILENAME = 'debug.log'
 logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
 
 
-class Swing(threading.Thread):
-    """docstring for Swing"""
-    def __init__(self, url):
-        super(Swing, self).__init__()
-        self.url = url
-    
-    def run(self):
-        browser = mechanize.Browser()
-        start_time = datetime.datetime.now()
-        # open the url
-        response = browser.open(self.url).get_data()
-        finish_time = datetime.datetime.now()
-        timedelta = (finish_time - start_time)
-        response_ms = timedelta.microseconds/1000
-        logging.debug("%s - %s" % (start_time, response_ms))
-        
-
-
 class Hammer(object):
     """Usage: Hammer(url="http://cool.biz", num_swings=10).run()"""
     def __init__(self, url, num_swings=5):
@@ -35,9 +17,26 @@ class Hammer(object):
     def run(self):
         for i in range(self.num_swings):
             Swing(self.url).start()
-    
+
+class Swing(threading.Thread):
+    """Used only by Hammer"""
+    def __init__(self, url):
+        super(Swing, self).__init__()
+        self.url = url
+
+    def run(self):
+        browser = mechanize.Browser()
+        start_time = datetime.datetime.now()
+        # open the url
+        response = browser.open(self.url).get_data()
+        finish_time = datetime.datetime.now()
+        timedelta = (finish_time - start_time)
+        response_ms = timedelta.microseconds/1000
+        logging.debug("%s - %s" % (start_time, response_ms))
+
+
     
     
 # If you like, you can uncomment the next two lines and invoke Hammer within this file:
-# my_url = "http://nin.ja/urlThing"
-# Hammer(url=my_url, num_swings=100).run()
+my_url = "http://kyle.tbxing.com/frontend_dev.php/test/index"
+Hammer(url=my_url, num_swings=100).run()
