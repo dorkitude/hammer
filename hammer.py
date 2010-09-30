f950ea046cc24d58b81d373694175ad1d6207aff
dorkitude <dork@dorkitude.com>
2010-09-30 13:37:32 -0700

    NOW functionally complete

--- hammer.py
+++ hammer.py
@@ -1,7 +1,40 @@
 import mechanize
-import thread
+import threading
+import logging
+import datetime
+
+LOG_FILENAME = 'debug.log'
+logging.basicConfig(filename=LOG_FILENAME,level=logging.DEBUG)
 
 
-url="http://kyle.tbxing.com/frontend_dev.php/test/index"
-browser = mechanize.Browser()
-response = self.browser.open(url)
+class Swing(threading.Thread):
+    """docstring for Swing"""
+    def __init__(self, url):
+        super(Swing, self).__init__()
+        self.url = url
+    
+    def run(self):
+        browser = mechanize.Browser()
+        start_time = datetime.datetime.now()
+        # open the url
+        response = browser.open(self.url).get_data()
+        finish_time = datetime.datetime.now()
+        timedelta = (finish_time - start_time)
+        response_ms = timedelta.microseconds/1000
+        logging.debug("%s - %s" % (start_time, response_ms))
+        
+
+
+class Hammer(object):
+    """Usage: Hammer(url="http://cool.biz", num_swings=10).run()"""
+    def __init__(self, url, num_swings=5):
+        super(Hammer, self).__init__()
+        self.url = url
+        self.num_swings = num_swings
+        
+    def run(self):
+        for i in range(self.num_swings):
+            Swing(self.url).start()
+    
+
+Hammer(url="http://kyle.tbxing.com/frontend_dev.php/test/index", num_swings=20).run()
